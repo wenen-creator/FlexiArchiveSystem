d2f2391157aee4570558d1f107e3e75a3dd100a4
wenen-creator <yixiangluntan@163.com>
2024-09-03 10:45:02 +0800

    新增设置根存档路径的接口

--- Engine/Unity/Script/Assist/Info/DataArchiveConstData.cs
+++ Engine/Unity/Script/Assist/Info/DataArchiveConstData.cs
@@ -12,5 +12,17 @@ namespace FlexiArchiveSystem
     public static partial class DataArchiveConstData
     {
         private static string AppPersistentDataPath = Application.persistentDataPath;
+
+        /// <summary>
+        /// there are some platform can require special persistent path for save data
+        /// example as weixin.
+        /// This is part of the SDK business layer, so the system doesn't want to take on any more responsibility.
+        /// please check persistent path is corrent on current paltform, if system with problem
+        /// </summary>
+        /// <param name="path"></param>
+        public static void SetPersistentDataPath(string path)
+        {
+            AppPersistentDataPath = path;
+        }
     }
 }
